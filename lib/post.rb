ce15b0b95d243983d153948ba4b61d2b384dc9a3
Lisa <60980933+LisaHJung@users.noreply.github.com>
2020-04-07 11:03:22 -0600

    Update post.rb

--- lib/post.rb
+++ lib/post.rb
@@ -1,18 +1,30 @@
+require 'pry'
+
 class Post
-    attr_accessor :title, :author
-  
-    @@all = []
-  
-    def initialize(title)
-      @title = title
-      @@all << self
+  attr_accessor :title, :author
+
+  @@all =[]
+
+  def initialize (title)
+    @title=title
+    @@all << self
+  end 
+
+  def self.all
+    @@all
+  end 
+
+    # objective
+        # Check to see if a post instance has author instance attached to it
+        # if self.author, it should return the name of the artist
+            # we can use Ruby chain as song.author yield an author instance 
+                # so we can chain it with .name to get @name from the instance.
+        # if the post in question does not include an author instance, then it returns nil.
+  def author_name
+    if self.author
+        return self.author.name 
+    else
+      return nil
     end
-  
-    def self.all
-      @@all
-    end
-  
-    def author_name
-      author.name if author
-    end
-  end
+  end 
+end 
