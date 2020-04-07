6470c8c610ca54e12c4c69a884da112b31de1cb4
Lisa <60980933+LisaHJung@users.noreply.github.com>
2020-04-07 10:48:44 -0600

    Update song.rb

--- lib/song.rb
+++ lib/song.rb
@@ -13,6 +13,12 @@ class Song
       @@all
     end
   
+    # objective
+        # Check to see if a song instance has artist instance attached to it
+        # if self.artist, it should return the name of the artist
+            # we can use Ruby chain as song.artist yield an artist instance 
+                # so we can chain it with .name to get @name from the isntance.
+        # if the song in question does not include an artist instance, then it returns nil. 
     def artist_name
         if self.artist
             return self.artist.name
